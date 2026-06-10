d783637ad4c2c3ce87e3bea535e40045a7e2080b
beardev-in <abhinav.edulakanti@gmail.com>
2026-06-10 15:48:01 +0530

    Remove obsolete greeting script

--- src/greet.js
+++
@@ -1,9 +0,0 @@
-function greet(name) {
-console.log("Application starting");
-  return `Hello, ${name}!`;
-}
-
-console.log(greet("Developer"));
-console.log("Welcome to Git");
-// History continues
-// End of file
